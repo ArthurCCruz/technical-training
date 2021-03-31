00003b390c5dc6613b6f33e2103e095f95097dca
ArthurCCruz <ccruz.arthur@gmail.com>
2021-03-31 16:06:36 -0300

    [ADD] added total due field for partners

--- library/models/partner.py
+++ library/models/partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class Partner(models.Model):
@@ -9,3 +9,13 @@ class Partner(models.Model):
     is_publisher = fields.Boolean(string="Is a Publisher", default=False)
 
     rental_ids = fields.One2many('library.rental', 'customer_id', string='Rentals')
+    total_due = fields.Float(compute="_compute_total_due", store=True)
+
+    @api.depends('rental_ids.state', 'rental_ids.total_price')
+    def _compute_total_due(self):
+        for record in self:
+            total_due = 0
+            for rental in record.rental_ids:
+                if rental.state != 'paid':
+                    total_due += rental.total_price
+            record.total_due = total_due
