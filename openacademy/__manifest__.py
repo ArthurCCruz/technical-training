a167bb82ac4feb517d132375d7dd4ead59a8b3ba
ArthurCCruz <ccruz.arthur@gmail.com>
2021-04-02 07:46:53 -0300

    [ADD] security file with groups and rules to apply on courses

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data':        [
+        "security/security.xml",
         "security/ir.model.access.csv",
         "views/course_views.xml",
         "views/session_views.xml",
